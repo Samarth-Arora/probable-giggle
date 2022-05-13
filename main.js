d096972a41f6c03635572f737998c3b773329ba5
Samarth-Arora <90255793+Samarth-Arora@users.noreply.github.com>
2022-05-13 11:36:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+//https://teachablemachine.withgoogle.com/models/spAA2N45g/
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/spAA2N45g/model.json', modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("result_label").innerHTML = 'i can Hear-' + results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'accuracy' + (results[0].confidence * 100).toFixed(2) + " %";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+
+        img1 = document.getElementById('1st animal');
+        img2 = document.getElementById('2nd animal');
+        img3 = document.getElementById('3rd animal');
+        img4 = document.getElementById('4th animal');
+
+        if (results[0].label == "meow") {
+            img1.src = 'woof-doggy.png';
+            img2.src = 'Cat.gif';
+            img3.src = 'Mooing Cow.png';
+            img4.src = 'lion-smile.png';
+        }
+
+        else if (results[0].label == "moo") {
+            img1.src = 'woof-doggy.png';
+            img2.src = 'Cat.png';
+            img3.src = 'Mooing cow.gif';
+            img4.src = 'lion-smile.png';
+        }
+
+        else if (results[0].label == "woof") {
+            img1.src = 'woof-doggy.gif';
+            img2.src = 'Cat.png';
+            img3.src = 'Mooing Cow.png';
+            img4.src = 'lion-smile.png';
+        }
+
+       else {
+            img1.src = 'woof-doggy.png';
+            img2.src = 'Cat.png';
+            img3.src = 'Mooing Cow.png';
+            img4.src = 'lion-smile.gif';
+        }
+
+    }
+
+
+}
